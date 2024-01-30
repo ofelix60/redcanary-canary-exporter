04f1ff896325b9ea9e8a8158247063a75d357211
Oscar Felix <ofelix@pointloma.edu>
2024-01-29 16:00:31 -0800

    v2.

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-# https://help.redcanary.com/hc/en-us/articles/1500007392202-Where-Do-I-Get-the-Linux-EDR-Docker-Container-Image-
-
-# IDK where to get the redcanary/canary-exporter.. 
-FROM redcanary/canary-exporter 
-COPY config/config.yaml /config.yaml
--- scripts/start_canary_exporter_linux.sh
+++ scripts/start_canary_exporter_linux.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # The default location to save the config.yaml file in Linux is: $HOME/canary-exporter/config.yaml. 
-docker run -d --volume $(pwd)/config/config.yaml:/config.yaml --volume $(pwd)/output:/output redcanary/canary-exporter
+# docker run -d --volume $(pwd)/config/config.yaml:/config.yaml --volume $(pwd)/output:/output redcanary/canary-exporter
+docker run -t --volume $HOME/canary-exporter/config.yaml:/config.yaml --volume $HOME/canary-exporter/output:/output redcanary/canary-exporter-ruby
